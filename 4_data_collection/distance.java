1b19b1972d59f92bd28895f2e0c36c32805d4612
ktottempudi <43048860+ktottempudi@users.noreply.github.com>
2019-03-24 14:27:11 -0400

    Add files via upload

---
+++ 4_data_collection/distance.java
@@ -0,0 +1,28 @@
+package gps_distance;
+
+public class distance {
+
+	public static void main(String[] args) {
+		System.out.print(distance1(40.52251, 40.52226, -74.4611, -74.46129));
+
+	}
+	
+	public static double distance1(double lat1, double lat2, double lon1,
+	        double lon2) {
+
+	    final int R = 6371; // Radius of the earth
+
+	    double latDistance = Math.toRadians(lat2 - lat1);
+	    double lonDistance = Math.toRadians(lon2 - lon1);
+	    double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+	            + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+	            * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+	    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	    double distance = R * c * 1000; // convert to meters
+
+	    distance = Math.pow(distance, 2);
+
+	    return Math.sqrt(distance);
+	}
+
+}
