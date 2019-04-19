657a227aea7980104f3add8baefb919cfb7f4d5e
Abhiek187 <achaudhuri2011@yahoo.com>
2019-04-19 17:53:48 -0400

    Add comments for clarity

--- 4_data_collection/ReadWrite/write_to_file.py
+++ 4_data_collection/ReadWrite/write_to_file.py
@@ -1,14 +1,16 @@
+# Run this in the background while testing printtest.html
 import random
 import time
 
 print("Server connected")
 
 while True:
+	# Keep running until user presses Ctrl+C (or Cmd+C)
 	try:
 		rand_num = random.randint(1, 100)
 
 		with open('numbers.txt', 'w') as num_file:
-			num_file.write(str(rand_num))
+			num_file.write(str(rand_num)) # Write a random number into numbers.txt
 
 		time.sleep(3)
 	except KeyboardInterrupt:
