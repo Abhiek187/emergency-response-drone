e22504af64a9c069981d30093a46ccacb4b208bc
VishalV97 <vv198@scarletmail.rutgers.edu>
2019-04-29 14:39:21 -0400

    Code for sensors

--- 1_code/controller/Obstacles/newright_sensor.py
+++ 1_code/controller/Obstacles/newright_sensor.py
@@ -35,7 +35,7 @@ while True:
 		pulse_duration = pulse_end_time - pulse_start_time
 		distance = round(pulse_duration * 17150, 2)
 
-		#write distance to a file
+		#write distance to a file so the webpage can use the data from the sensors
 		with open('rightsensor.txt', 'w') as num_file:
 			num_file.write(str(distance))
 	#allow keyboard interrupt to stop program
